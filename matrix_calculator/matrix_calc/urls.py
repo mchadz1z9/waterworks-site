39751e5bc158c77a53486cc9deb4c8466d8ac466
mchadz1z9 <mitchell.chad2@outlook.com>
2026-04-15 22:43:05 -0400

    auto: changes after 3 edits [2026-04-15 22:43:04]

--- matrix_calculator/matrix_calc/urls.py
+++ matrix_calculator/matrix_calc/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('calculator/', include('calculator.urls')),
-    path('', include('calculator.urls')),
+    path('', RedirectView.as_view(url='/calculator/', permanent=False)),
 ]
